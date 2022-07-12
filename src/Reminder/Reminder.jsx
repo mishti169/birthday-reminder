b7e5c4c846dc51291ad8b506f262979b80f951cd
MANISHA <manishapanchal169@gmail.com>
2022-07-12 16:57:14 +0530

    create class base component

---
+++ src/Reminder/Reminder.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Profile from '../Profile/Profile'
+class Reminder extends Component{
+  render(){
+    return(
+      <div>
+        <Profile/>
+      </div>
+    )
+  }
+}
+
+export default Reminder;

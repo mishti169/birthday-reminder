e323651b66f238a5d0124e7d708355c46e3b16dc
MANISHA <manishapanchal169@gmail.com>
2022-07-12 17:06:06 +0530

    make single profile hard coded card

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -4,7 +4,13 @@ class Profile extends Component{
 
   render(){
     return(
-      <div>hello</div>
+      <div>
+        <img src='https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg' width={160}/>
+        <div>
+          <span>Mishti Panchal</span>
+          <span>23 Years</span>
+        </div>
+      </div>
     )
   }
 }
